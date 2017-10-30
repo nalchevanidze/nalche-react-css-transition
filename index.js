1f2f6e8f3cb4d0c8a1f6a6fc3efe1b169ceec56b
daviti nalchevanidze <d.nalchevanidze@gmail.com>
2017-10-30 22:54:00 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-import CSSTransitionMember from "./dist/CSSTransitionMember";
-import CSSTransitionSession from "./dist/CSSTransitionSession";
-exports.CSSTransitionMember = CSSTransitionMember;
-exports.CSSTransitionSession = CSSTransitionSession;
-export default CSSTransitionSession;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _CSSTransitionMember = require("./dist/CSSTransitionMember");
+
+var _CSSTransitionMember2 = _interopRequireDefault(_CSSTransitionMember);
+
+var _CSSTransitionSession = require("./dist/CSSTransitionSession");
+
+var _CSSTransitionSession2 = _interopRequireDefault(_CSSTransitionSession);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.CSSTransitionMember = _CSSTransitionMember2.default;
+exports.CSSTransitionSession = _CSSTransitionSession2.default;
+exports.default = _CSSTransitionSession2.default;
